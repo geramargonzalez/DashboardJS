c6fefa3ec86c456d3f233e6d070dd249cf6c712b
Gerardo Gonzalez <gerardo.gonzalez@techfino.com>
2024-06-10 16:03:54 -0300

    skeleton lastest invoice

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -4,7 +4,7 @@ import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { montserrat } from '@/app/ui/fonts';
 import { Suspense } from 'react';
 import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 export default async function Page() {
 
  
@@ -33,7 +33,7 @@ export default async function Page() {
         <Suspense fallback={< RevenueChartSkeleton />}>
           <RevenueChart/>
         </Suspense>
-        <Suspense fallback={< RevenueChartSkeleton />}>
+        <Suspense fallback={< LatestInvoicesSkeleton  />}>
           <LatestInvoices/>
         </Suspense>
       </div>
