ffff85fbae598ea8b1b6b74469d422bd11c2dab6
Gerardo Gonzalez <gerardo.gonzalez@techfino.com>
2024-06-17 16:42:44 -0300

    logic create invoice

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,11 +1,29 @@
 'use server';
+import {z} from 'zod'
+import { Invoice } from './definitions';
+import { sql } from '@vercel/postgres';
+
+const CreateInvoiceFormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+});
+
+const CreateInvSheVal = CreateInvoiceFormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvSheVal.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
-  // Test it out:
-  console.log(rawFormData);
+  });
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
 }
--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -21,8 +21,6 @@ export type Invoice = {
   customer_id: string;
   amount: number;
   date: string;
-  // In TypeScript, this is called a string union type.
-  // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
   status: 'pending' | 'paid';
 };
 
@@ -86,3 +84,5 @@ export type InvoiceForm = {
   amount: number;
   status: 'pending' | 'paid';
 };
+
+
