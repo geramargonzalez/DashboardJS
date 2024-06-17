ab5945f871e220c30cd91249d2b2a6e877340d02
Gerardo Gonzalez <gerardo.gonzalez@techfino.com>
2024-06-16 22:49:51 -0300

    correct create invoice page

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -102,7 +102,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         <Link
           href="/dashboard/invoices"
           className="flex h-10 items-center rounded-lg bg-gray-100 px-4 text-sm font-medium text-gray-600 transition-colors hover:bg-gray-200"
-        >
+         >
           Cancel
         </Link>
         <Button type="submit">Create Invoice</Button>
