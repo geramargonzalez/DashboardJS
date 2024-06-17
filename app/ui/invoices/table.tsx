6f4d0419e2182bd635b1086d45187012a2df6514
Gerardo Gonzalez <gerardo.gonzalez@techfino.com>
2024-06-16 21:52:54 -0300

    add pagination an delaying search params

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -22,7 +22,7 @@ export default async function InvoicesTable({
               <div
                 key={invoice.id}
                 className="mb-2 w-full rounded-md bg-white p-4"
-              >
+                >
                 <div className="flex items-center justify-between border-b pb-4">
                   <div>
                     <div className="mb-2 flex items-center">
--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,22 +2,26 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
   
   const searchParams = useSearchParams();
   const pathname = usePathname();
   const { replace } = useRouter();
+  const delaySearch = 400;
   
-  function handleSearch(term: string) {
+  const handleSearch = useDebouncedCallback((term: string) => {
     const params = new URLSearchParams(searchParams);
+    params.set('page', '1');
     if (term) {
       params.set('query', term);
     } else {
       params.delete('query');
     }
     replace(`${pathname}?${params.toString()}`);
-  }
+  }, delaySearch);
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
