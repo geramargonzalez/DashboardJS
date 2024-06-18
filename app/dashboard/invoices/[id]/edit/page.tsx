55c7e3c2f89eee5a3ea083003418c5eb0d52435c
Gerardo Gonzalez <gerardo.gonzalez@techfino.com>
2024-06-17 22:19:02 -0300

    update

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -9,7 +9,6 @@ export default async function Page({ params }: { params: { id: string } }) {
         fetchInvoiceById(id),
         fetchCustomers(),
       ]);
-
       if (!invoice) {
         notFound();
       }
