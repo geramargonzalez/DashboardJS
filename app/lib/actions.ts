0cd8b0223cfe8792f0b37875ca1b04ceb15b2a35
Gerardo Gonzalez <gerardo.gonzalez@techfino.com>
2024-06-18 14:35:25 -0300

    add server validation field

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,24 +7,47 @@ import { redirect } from 'next/navigation';
 
 const CreateInvoiceFormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
+
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
 
 const CreateInvSheVal = CreateInvoiceFormSchema.omit({ id: true, date: true });
 const UpdateInvoice = CreateInvoiceFormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvSheVal.parse({
+export async function createInvoice(prevState: State, formData: FormData) {
+  const validatedFields = CreateInvSheVal.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
+
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+  const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
   const date = new Date().toISOString().split('T')[0];
-
+  
   try {
     await sql`
       INSERT INTO invoices (customer_id, amount, status, date)
