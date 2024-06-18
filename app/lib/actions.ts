4c4977f95aeafd7c4759e2552fbd8132eb7bd1f0
Gerardo Gonzalez <gerardo.gonzalez@techfino.com>
2024-06-17 21:58:33 -0300

    updating Script

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,6 +14,7 @@ const CreateInvoiceFormSchema = z.object({
 });
 
 const CreateInvSheVal = CreateInvoiceFormSchema.omit({ id: true, date: true });
+const UpdateInvoice = CreateInvoiceFormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
   const { customerId, amount, status } = CreateInvSheVal.parse({
@@ -34,4 +35,28 @@ export async function createInvoice(formData: FormData) {
   // redirect Module
   redirect('/dashboard/invoices');
 
+}
+
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+export async function deleteInvoice(id: string) {
+  await sql`DELETE FROM invoices WHERE id = ${id}`;
+  revalidatePath('/dashboard/invoices');
 }
