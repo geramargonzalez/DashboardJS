bfe2b05b76bf0ad17222b6da546a1986edd9b1d5
Gerardo Gonzalez <gerardo.gonzalez@techfino.com>
2024-06-26 12:20:47 -0300

    add env variable with openSS

--- auth.ts
+++ auth.ts
@@ -26,16 +26,13 @@ export const { auth, signIn, signOut } = NextAuth({
         const parsedCredentials = z
           .object({ email: z.string().email(), password: z.string().min(6) })
           .safeParse(credentials);
- 
           if (parsedCredentials.success) {
             const { email, password } = parsedCredentials.data;
             const user = await getUser(email);
             if (!user) return null;
             const passwordsMatch = await bcrypt.compare(password, user.password);
-   
             if (passwordsMatch) return user;
           }
-   
           console.log('Invalid credentials');
           return null;
       },
